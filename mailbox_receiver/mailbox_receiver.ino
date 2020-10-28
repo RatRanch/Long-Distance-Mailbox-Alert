e14633b0a7cda10838878ba0581c73f39323d01c
RatRanch <ratranch@mac.com>
2020-10-28 16:07:45 -0400

    Update mailbox_receiver.ino

--- mailbox_receiver/mailbox_receiver.ino
+++ mailbox_receiver/mailbox_receiver.ino
@@ -22,57 +22,6 @@
 #define RFM95_RST 4
 #define RFM95_INT 7
 
-
-/* for feather m0 RFM9x
-#define RFM95_CS 8
-#define RFM95_RST 4
-#define RFM95_INT 3
-*/
-
-/* for shield 
-#define RFM95_CS 10
-#define RFM95_RST 9
-#define RFM95_INT 7
-*/
-
-/* Feather 32u4 w/wing
-#define RFM95_RST     11   // "A"
-#define RFM95_CS      10   // "B"
-#define RFM95_INT     2    // "SDA" (only SDA/SCL/RX/TX have IRQ!)
-*/
-
-/* Feather m0 w/wing 
-#define RFM95_RST     11   // "A"
-#define RFM95_CS      10   // "B"
-#define RFM95_INT     6    // "D"
-*/
-
-//#if defined(ESP8266)
-  /* for ESP w/featherwing */ 
-//  #define RFM95_CS  2    // "E"
-//  #define RFM95_RST 16   // "D"
-//  #define RFM95_INT 15   // "B"
-
-//#elif defined(ESP32)  
-  /* ESP32 feather w/wing */
-//  #define RFM95_RST     27   // "A"
-//  #define RFM95_CS      33   // "B"
-//  #define RFM95_INT     12   //  next to A
-
-//#elif defined(NRF52)  
-  /* nRF52832 feather w/wing */
-//  #define RFM95_RST     7   // "A"
-//  #define RFM95_CS      11   // "B"
-//  #define RFM95_INT     31   // "C"
-  
-//#elif defined(TEENSYDUINO)
-  /* Teensy 3.x w/wing */
-//  #define RFM95_RST     9   // "A"
-//  #define RFM95_CS      10   // "B"
-//  #define RFM95_INT     4    // "C"
-//#endif
-
-
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
 
